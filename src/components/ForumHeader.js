defa1a1a60e2e937f305c46c2c03f03495b6b4fb
create-rarac <big.rarac@gmail.com>
2025-08-29 21:00:11 +0200

    Create ForumHeader component

---
+++ src/components/ForumHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function ForumHeader() {
+  return (
+    <div className="ff-header">
+      <span className="ff-title">FlashForum 2009</span>
+      <span className="ff-nav">[Home] [Search] [Help]</span>
+    </div>
+  );
+}
